5abbd07f8601fb096da25bab99985285348cf532
EvaldasStremas <evaldas.stremas123@gmail.com>
2021-05-21 11:50:01 +0300

    added main.py

---
+++ Example-1/main.py
@@ -0,0 +1,11 @@
+from simple_device import SimpleDevice
+
+device = SimpleDevice()
+
+device.on_event('pin_entered')
+device.on_event('device_locked')
+device.on_event('device_locked')
+
+print(device.state)
+
+# device.on_event('')
