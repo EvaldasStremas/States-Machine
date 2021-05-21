d9ceeed645a2223be8162650a66b5fafb33f6f0b
EvaldasStremas <evaldas.stremas123@gmail.com>
2021-05-21 14:06:19 +0300

    init

--- Example-1/main.py
+++ Example-1/main.py
@@ -1,3 +1,7 @@
+'''
+https://dev.to/karn/building-a-simple-state-machine-in-python
+'''
+
 from simple_device import SimpleDevice
 
 device = SimpleDevice()
@@ -5,7 +9,4 @@ device = SimpleDevice()
 device.on_event('pin_entered')
 device.on_event('device_locked')
 device.on_event('device_locked')
-
 print(device.state)
-
-# device.on_event('')
